9c71c668670601890ccb7766eece5fa3c3307b3b
shashipreetham <spdevipi@gmail.com>
2026-04-02 17:12:15 +0530

    Trail1

--- src/main/java/com/ust/pos/PosApplication.java
+++ src/main/java/com/ust/pos/PosApplication.java
@@ -1,9 +1,11 @@
 package com.ust.pos;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
 import org.springframework.boot.security.autoconfigure.actuate.web.servlet.ManagementWebSecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -17,6 +19,10 @@ public class PosApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PosApplication.class, args);
+    }
+    @Bean
+    ModelMapper getModelMapper(){
+        return new ModelMapper();
     }
 
 }
