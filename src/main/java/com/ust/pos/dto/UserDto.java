2913c140ab9a5e3114cfed74f9dfffa65983f585
Lekhya <308337@ust.com>
2026-04-02 12:16:11 +0530

    New files update!

---
+++ src/main/java/com/ust/pos/dto/UserDto.java
@@ -0,0 +1,20 @@
+package com.ust.pos.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class UserDto {
+    private int id;
+    private String name;
+    private String email;
+    private String phoneNo;
+    private String userName;
+    private String password;
+    private int age;
+    private Date dateOfBirth;
+
+}
---
+++ src/main/java/com/ust/pos/model/User.java
@@ -0,0 +1,26 @@
+package com.ust.pos.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String email;
+    private String phoneNo;
+    private String userName;
+    private String password;
+    private int age;
+    private Date dateOfBirth;
+}
