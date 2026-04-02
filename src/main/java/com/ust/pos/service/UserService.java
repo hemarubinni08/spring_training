2dc03f6e13b773a17f79f448fc8832ce1943c983
Rohit Raj Saji <rohitraj.saji@ust.com>
2026-04-02 16:52:15 +0530

    Service layer with impl - 1

---
+++ src/main/java/com/ust/pos/service/UserService.java
@@ -0,0 +1,19 @@
+package com.ust.pos.service;
+
+import com.ust.pos.dto.UserDto;
+
+import java.util.List;
+
+public interface UserService {
+
+    UserDto createUser(UserDto user);
+
+    UserDto getUserById(Long id);
+
+    List<UserDto> getAllUsers();
+
+    UserDto updateUser(Long id, UserDto user);
+
+    void deleteUser(Long id);
+
+}
---
+++ src/main/java/com/ust/pos/service/impl/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package com.ust.pos.service.impl;
+
+import com.ust.pos.dto.UserDto;
+import com.ust.pos.service.UserService;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public UserDto createUser(UserDto user) {
+        return null;
+    }
+
+    @Override
+    public UserDto getUserById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<UserDto> getAllUsers() {
+        return List.of();
+    }
+
+    @Override
+    public UserDto updateUser(Long id, UserDto user) {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+
+    }
+}
